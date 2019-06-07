965258e48ed754a73e1b1fe20efcadd1bb06b7f4
Liz <jesusridesthebus@gmail.com>
2019-06-07 13:27:13 -0700

    add api call

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,5 +1,19 @@
 import $ from 'jquery';
 
-export function add(a, b) {
-    return a + b;
+export class Doctor {
+	getDoctor() {
+		return new Promise((resolve, reject) => {
+			let request = new XMLHttpRequest();
+			let url = `https://developer.betterdoctor.com/`;
+			request.onload = function() {
+				if(this.status === 200) {
+					resolve(request.response);
+				} else {
+					reject(Error(request.statusText));
+				}
+			}
+			request.open("GET", url, true);
+			request.send();
+		});
+	}
 }
