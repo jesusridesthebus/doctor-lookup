978fab1f8264a7256bd4acc17b79e35a2e4d21b2
Student <example@example.com>
2019-06-07 16:47:01 -0700

    fix main.js logic for displaying doctor names

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
 
 export class Doctor {
-	getDoctor(doctorName) {
+	getDoctor(value, category) {
 		return new Promise((resolve, reject) => {
 			let request = new XMLHttpRequest();
-			let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+			let url = `https://api.betterdoctor.com/2016-03-01/doctors?${category}=${value}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 			console.log(url);
 			request.onload = function() {
 				if(this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -6,22 +6,37 @@ import $ from 'jquery';
 // import './css/styles.css';
 
 $(document).ready(function() {
-  $("#doctor-name").click(() => {
+  $("#name").click(() => {
+    $("#show-doctors").empty();
     event.preventDefault();
-    let doctor = $("#input-doctor-name").val();
+    let doctor = $(".input").val();
+    console.log("doctor", doctor);
     let doctorLookup = new Doctor();
-    let promise = doctorLookup.getDoctor(doctor);
-
+    let promise = doctorLookup.getDoctor(doctor, $("#name").attr(`id`));
+    console.log("this", $(this));
 
     promise.then((response) => {
       let body = JSON.parse(response);
-      for(i = 0; i < body.data.length; i++) {
-        console.log(body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name);
-
+      for(let i = 0; i < body.data.length; i++) {
+        $("#show-doctors").append(body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + ' ' + body.data[i].profile.title + ' ' + 'Accepts New Patients?: ' + body.data[i].practices[0].accepts_new_patients + ' ' + body.data[i].practices[0].phones[0].number + ' ' + body.data[i].practices[0].visit_address.street + ' ' + '<br>');
       }
-      console.log(body.data);
-      // let doctor = body.doctor;
-      // $('.find-doctor').text;
+    });
+  });
+
+  $("#query").click(() => {
+    $("#show-doctors").empty();
+    event.preventDefault();
+    let doctor = $(".query").val();
+    console.log("doctor", doctor);
+    let doctorLookup = new Doctor();
+    let promise = doctorLookup.getDoctor(doctor, $("#query").attr(`id`));
+    console.log("this", $(this));
+
+    promise.then((response) => {
+      let body = JSON.parse(response);
+      for(let i = 0; i < body.data.length; i++) {
+        $("#show-doctors").append(body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + ' ' + body.data[i].profile.title + ' ' + 'Accepts New Patients?: ' + body.data[i].practices[0].accepts_new_patients + ' ' + body.data[i].practices[0].phones[0].number + ' ' + body.data[i].practices[0].visit_address.street + ' ' + '<br>');
+      }
     });
   });
 });
