5254edff4b1bf248aa4afa0c6967871576425379
Student <example@example.com>
2019-06-07 17:25:18 -0700

    add error messages

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { Doctor } from './doctor-lookup.js';
 import $ from 'jquery';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import './styles.css';
 
 $(document).ready(function() {
   $("#name").click(() => {
@@ -17,10 +17,17 @@ $(document).ready(function() {
 
     promise.then((response) => {
       let body = JSON.parse(response);
+      if(body.data.length === 0) {
+        $('#show-doctors').append("No doctors match your search.");
+      }
       for(let i = 0; i < body.data.length; i++) {
         $("#show-doctors").append(body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + ', ' + body.data[i].profile.title + '. ' + 'Accepts New Patients?: ' + body.data[i].practices[0].accepts_new_patients + '. ' + body.data[i].practices[0].phones[0].number + '. ' + body.data[i].practices[0].visit_address.street + '. ' + '<br>');
       }
-    });
+    },(error) => {
+      $(".error").show();
+      $('#show-doctors').append(`<p>There was an error processing your request: ${error.message}</p>`);
+    }
+  );
   });
 
   $("#query").click(() => {
@@ -34,9 +41,15 @@ $(document).ready(function() {
 
     promise.then((response) => {
       let body = JSON.parse(response);
+      if(body.data.length === 0) {
+        $('#show-doctors').append("No doctors match your search.");
+      }
       for(let i = 0; i < body.data.length; i++) {
         $("#show-doctors").append(body.data[i].profile.first_name + ' ' + body.data[i].profile.last_name + ', ' + body.data[i].profile.title + '. ' + 'Accepts New Patients?: ' + body.data[i].practices[0].accepts_new_patients + '. ' + body.data[i].practices[0].phones[0].number + '. ' + body.data[i].practices[0].visit_address.street + '. ' + '<br>');
       }
+    }, (error) => {
+      $(".error").show();
+      $('#show-doctors').append(`<p>There was an error processing your request: ${error.message}</p>`);
     });
   });
 });
